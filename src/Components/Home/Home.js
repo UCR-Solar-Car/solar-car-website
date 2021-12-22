8b44ee35a3183b276ab8d46da0cb6fe1598eacba
shahdivyank <divyanks.shah.2016@gmail.com>
2021-12-22 13:31:13 -0800

    update to arrow func

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
-function Home() {
+const Home = () => {
     return (
         <div>
-            <center>
-                <img src="https://www.gaithersburgdental.com/wp-content/uploads/2016/10/orionthemes-placeholder-image.png" width = "80%"></img>
-            </center>
+            
         </div>
     )
 }
